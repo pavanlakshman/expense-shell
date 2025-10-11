5bedb7f100981f8067093cee3cf5dd92cf722a03
Pavan <pavanlakshman.a@gmail.com>
2025-10-11 11:07:56 +0530

    backend modified

--- backend.sh
+++ backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h db.pavandevops.online -uroot -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 172.31.30.86 -uroot -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
